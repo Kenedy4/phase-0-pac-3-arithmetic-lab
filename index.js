f5e13292725fa09f3bacb6640e458575c691a52b
kenedy4 <kennedymusyoki8@gmail.com>
2024-06-22 09:04:25 +0300

    Added the add function

---
+++ index.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+  return a + b;
+}
+function add(a, b) {
+  return a - b;
+}
+function add(a, b) {
+  return a * b;
+}
+function add(a, b) {
+  return a / b;
+}
+function add(a) {
+  return a++;
+}
